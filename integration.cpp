2027945284e1ae1e2943869b1be57cd5997e566b
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-11-18 18:01:00 -0500

    added structures for patient and doctor records

--- integration.cpp
+++ integration.cpp
@@ -3,6 +3,29 @@
 
 using namespace std;
 
+struct patient{
+	double bloodPressure;
+	double temperature;
+	double heartrate;
+	double oxygen;
+	int age;
+	char* name;
+	char* doctor;
+} ;
+
+//assume that this stucture stores identification information accuracy threasholds as doubles between 0 and 1 inclusively
+//Identification accuracy will be recieved by the program to compare to threadholds
+//For status, a True indicates the doctor is in office and availible.
+struct doctor{
+	double fingerprint;
+	double iris;
+	double voice;
+	double signature;
+	char* name;
+	bool status;
+} ;
+
+
 int main(int argc, char **argv) {
 
 
